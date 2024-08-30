d79d2cbc3ba6aeed90e2e0bb76b1bcf1a194046b
Urpagin <urpagin@proton.me>
2024-08-30 08:37:41 +0200

    update

--- init.lua
+++ init.lua
@@ -159,6 +159,11 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- CUMSTOM KEYMEPSAA
+
+local bufopts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, bufopts)
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
