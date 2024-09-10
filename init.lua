584e48f563d8920c7334702d3c9da4808f9a8e8f
urpagin <urpagin@domain.com>
2024-09-11 00:50:37 +0200

    python

--- init.lua
+++ init.lua
@@ -612,6 +612,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
+        --ruff = {},
         pyright = {},
         rust_analyzer = {},
         html = {},
@@ -654,7 +655,6 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'pyright', -- Used to format Python code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
